ca9a01bf1631445cc0a1116f92f2faa627d5c5cb
Bassem Saeed <bassemsaeed620@gmail.com>
2022-08-28 02:02:49 +0200

    edits

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const workoutRoutes = require("./Routes/workouts");
 const userRoutes = require("./Routes/User");
 
@@ -9,6 +11,9 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
