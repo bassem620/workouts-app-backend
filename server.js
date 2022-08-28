c0c9709f95077d7f226d95d1deef8183ed404dcd
Bassem Saeed <bassemsaeed620@gmail.com>
2022-08-28 02:17:41 +0200

    edits

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 const workoutRoutes = require("./Routes/workouts");
 const userRoutes = require("./Routes/User");
 
@@ -9,6 +10,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
